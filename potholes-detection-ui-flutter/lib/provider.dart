c2bfb0820b0e13d079e84738976ac11c641a5cda
adriansarbu2001 <sarbuadrian2001@gmail.com>
2022-12-12 18:56:54 +0200

    API and UI

---
+++ potholes-detection-ui-flutter/lib/provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+class PotholesDetectionProvider extends ChangeNotifier {
+  final String baseUri = "http://10.0.2.2:5000";
+  Image? image = null;
+  bool loading = false;
+  String? error = null;
+
+  Future<void> postPhoto(File imageFile) async {
+    image = null;
+    error = null;
+    loading = true;
+    notifyListeners();
+    try {
+      var response = await http.post(
+        Uri.parse("$baseUri/potholes-detection"),
+        body: await imageFile.readAsBytes(),
+      );
+
+      image = Image.memory(response.bodyBytes);
+      loading = false;
+      notifyListeners();
+    } catch (e) {
+      error = e.toString();
+      loading = false;
+      notifyListeners();
+    }
+  }
+}
