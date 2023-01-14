e2f7396df9d268fddc48a11a50409e0d6c8a8e4b
Sarbu Adrian-Mihai <adriansarbu2001@yahoo.ro>
2023-01-14 20:54:58 +0200

    commit

--- potholes-detection/main-training.py
+++ potholes-detection/main-training.py
@@ -1,27 +1,18 @@
 import os
 import random
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from itertools import chain
-from skimage.io import imread, imshow, concatenate_images
 from skimage.transform import resize
-from skimage.morphology import label
-from sklearn.model_selection import train_test_split
 
-import tensorflow as tf
-
-from keras.models import Model, load_model
-from keras.layers import Input, BatchNormalization, Activation, Dense, Dropout
-from keras.layers.core import Lambda, RepeatVector, Reshape
+from keras.models import Model
+from keras.layers import Input, BatchNormalization, Activation, Dropout
 from keras.layers.convolutional import Conv2D, Conv2DTranspose
-from keras.layers.pooling import MaxPooling2D, GlobalMaxPool2D
-from keras.layers.merging import concatenate, add
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merging import concatenate
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import array_to_img, img_to_array, load_img
+from keras.utils import img_to_array, load_img
 
 """
 gpus = tf.config.experimental.list_physical_devices('GPU')
