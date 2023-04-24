9a500e595e5a824d228f905bba3b5f8e67cbe0a0
Sarbu Adrian-Mihai <adriansarbu2001@yahoo.ro>
2023-04-24 10:40:24 +0300

    commit

--- potholes_detection/main_user_testing.py
+++ potholes_detection/main_user_testing.py
@@ -16,8 +16,8 @@ model = load_model('custom_trained_model.h5', compile=False)
 
 # model.summary()
 
-img = load_img("test2.png", color_mode="rgb")
-label = load_img("test2_real_label.png", color_mode="grayscale")
+img = load_img("test.png", color_mode="rgb")
+label = load_img("test_real_label.png", color_mode="grayscale")
 x_img = img_to_array(img)
 x_img = resize(x_img, (IM_HEIGHT, IM_WIDTH, 3), mode="constant", preserve_range=True)
 x_img = x_img / 255.0
