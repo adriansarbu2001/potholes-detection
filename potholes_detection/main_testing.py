e25d2a88b3b2dd3ad20f9d0c16e7b9ef7902c1d5
Sarbu Adrian-Mihai <adriansarbu2001@yahoo.ro>
2023-06-02 15:50:27 +0300

    commit

--- potholes_detection/main_testing.py
+++ potholes_detection/main_testing.py
@@ -3,7 +3,7 @@ import cv2
 
 import numpy as np
 import tensorflow as tf
-from keras.metrics import MeanIoU, Accuracy
+from keras.metrics import MeanIoU, Precision, Recall
 from keras.models import load_model
 
 from potholes_detection.utils.constants import IM_HEIGHT, IM_WIDTH
@@ -21,10 +21,11 @@ x_generator, y_generator = read_images(rgb_path="data/potholes_on_road/validatio
 test_generator = zip_generator(x_generator=x_generator, y_generator=y_generator)
 
 # load the best model
-model = load_model("saved_models/model.h5", compile=False)
+model = load_model("saved_models/model_v3.h5", compile=False)
 
 test_iou_metric = MeanIoU(num_classes=2)
-test_acc_metric = Accuracy()
+test_prec_metric = Precision()
+test_rec_metric = Recall()
 
 
 @tf.function
@@ -40,7 +41,8 @@ def test_step(x, y):
     y_pred = closing2d(input=y_pred, kernel=kernel)
 
     test_iou_metric.update_state(y, y_pred)
-    test_acc_metric.update_state(y, y_pred)
+    test_prec_metric.update_state(y, y_pred)
+    test_rec_metric.update_state(y, y_pred)
 
 
 print("Running evaluation...")
@@ -49,9 +51,12 @@ for x_batch_val, y_batch_val in test_generator:
     test_step(x_batch_val, y_batch_val)
 
 test_iou = test_iou_metric.result()
-test_acc = test_acc_metric.result()
+test_prec = test_prec_metric.result()
+test_rec = test_rec_metric.result()
 test_iou_metric.reset_states()
-test_acc_metric.reset_states()
+test_prec_metric.reset_states()
+test_rec_metric.reset_states()
 print("Test meanIoU: %.4f" % (float(test_iou),))
-print("Test accuracy: %.4f" % (float(test_acc),))
+print("Test precision: %.4f" % (float(test_prec),))
+print("Test recall: %.4f" % (float(test_rec),))
 print("Time taken: %.2fs" % (time.time() - start_time))
